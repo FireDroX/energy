3c65df7ad10eb9bd1d7efe94cb3356e1cda34f46
Seoo <153709278+XeTr0S@users.noreply.github.com>
2026-05-06 12:41:35 +0200

    test

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Monster</title>
-  <script src="app.js" defer></script>
-</head>
-<body>
-  <?php require_once './components/navbar.php' ?>
-  <h1>Titre</h1>
-  <button id="fetch">Fetch</button>
-</body>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Monster</title>
+    <script src="app.js" defer></script>
+  </head>
+
+  <body>
+    <?php require_once './components/navbar.php' ?>
+    <h1>Titre</h1>
+    <button id="fetch">Fetch</button>
+  </body>
 </html>
