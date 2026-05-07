434ec58a4cbda58027cc17b6290e794c1d006994
Seoo <153709278+XeTr0S@users.noreply.github.com>
2026-05-07 16:52:12 +0200

    Refactor navbar for user session display and add account/logout pages

--- components/navbar.php
+++ components/navbar.php
@@ -1,31 +1,44 @@
 <?php
-
 session_start();
-
-if (isset($_SESSION['user'])) {
-    echo "Connecté en tant que : " . $_SESSION['user']['pseudo'];
-} else {
-    echo "Utilisateur non connecté";
-}
 ?>
 
 <nav class="navbar navbar-dark bg-dark navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="/">Monster Energy</a>
+
     <div class="collapse navbar-collapse center" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
         <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/">Home</a>
+          <a class="nav-link active" href="/">Home</a>
         </li>
+
         <li class="nav-item">
-            <a class="nav-link" href="/search">Search</a>
+          <a class="nav-link" href="/search">Search</a>
         </li>
-        <li class="nav-item">
+
+        <?php if (isset($_SESSION['user'])): ?>
+
+          <li class="nav-item">
+            <a class="nav-link" href="/account">Compte</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="/logout">Déconnexion</a>
+          </li>
+
+        <?php else: ?>
+
+          <li class="nav-item">
             <a class="nav-link" href="/login">Login</a>
-        </li>
+          </li>
+
+        <?php endif; ?>
+
         <li class="nav-item">
-            <a class="nav-link" href="/">Contact</a>
+          <a class="nav-link" href="/">Contact</a>
         </li>
+
       </ul>
     </div>
   </div>
---
+++ logout/index.php
@@ -0,0 +1,42 @@
+<?php
+
+session_start();
+
+$_SESSION = [];
+
+session_destroy();
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Déconnexion</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="bg-light">
+
+    <div class="container">
+        <div class="row justify-content-center align-items-center vh-100">
+            <div class="col-md-6">
+
+                <div class="card shadow p-4 text-center">
+                    <h1 class="mb-3">Déconnexion réussie</h1>
+
+                    <p class="mb-4">
+                        Vous avez bien été déconnecté.
+                    </p>
+
+                    <a href="/" class="btn btn-dark">
+                        Retour à l'accueil
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
