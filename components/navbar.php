067b7ced8c74e516eb42da049662e0934de3907f
FireDroX <FireDroX@gmail.com>
2026-05-08 11:29:09 +0200

    Refactor navbar component: update styles, restructure HTML, and enhance user experience

--- components/navbar.php
+++ components/navbar.php
@@ -2,52 +2,82 @@
 
 session_start();
 
-if (isset($_SESSION['user'])) {
-    echo "Connecté en tant que : " . $_SESSION['user']['pseudo'];
-} else {
-    echo "Utilisateur non connecté";
-}
 ?>
 
-<link rel="stylesheet" href="/css/navbar_style.css">
-
+<link rel="stylesheet" href="/styles/navbar.css">
 <nav class="monster-navbar">
-  <div class="monster-logo">
-    <a href="/">Monster Energy</a>
-  </div>
-
-  <ul class="monster-links me-auto">
-
-    <li>
-      <a href="/">Home</a>
-    </li>
-
-    <li>
-      <a href="/search">Search</a>
-    </li>
-
-    <li>
-      <a href="/">Contact</a>
-    </li>
+  <div class="navbar-left">
+    <h4 class="monster-logo">
+      <a href="/">
+        <img class="navbar-logo" src="/favicon.png" alt="Logo">
+        <span>Monster Energy</span>
+      </a>
+    </h4>
+    <ul class="monster-links me-auto">
+      <li>
+        <a href="/">
+          <span>Home</span>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+          </svg>
+        </a>
+      </li>
+      <li>
+        <a href="/search">
+          <span>Recherche</span>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+          </svg>  
+        </a>
+      </li>
+      <li>
+        <a href="/">
+          <span>Contact</span>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+          </svg>
+        </a>
+      </li>
     </ul>
-  
-    <ul class="monster-links">
-    <?php if (isset($_SESSION['user'])): ?>
-
+  </div>
+  <ul class="monster-links">
+  <?php if (isset($_SESSION['user'])) { ?>
+    <li>
+      <a href="/account">
+        <span>Compte</span>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+        </svg>
+      </a>
+    </li>
+    <?php if($_SESSION['user']['role'] == 1 || $_SESSION['user']['role'] == 3) { ?>
       <li>
-        <a href="/account">Compte</a>
+        <a href="/">
+          <span>Panel</span>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+          </svg>
+        </a>
       </li>
-
-      <li>
-        <a href="/logout">Déconnexion</a>
-      </li>
-
-    <?php else: ?>
-
-      <li>
-        <a href="/login">Login</a>
-      </li>
-
-    <?php endif; ?>
+    <?php } ?>
+    <li>
+      <a href="/logout">
+        <span>Déconnexion</span>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9" />
+        </svg>
+      </a>
+    </li>
+  <?php } else { ?>
+    <li>
+      <a href="/login">
+        <span>Login</span>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
+        </svg>
+      </a>
+    </li>
+  <?php } ?>
   </ul>
 </nav>
