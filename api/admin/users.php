c9ad7e238a01c12e0e104a858681978a73c9d344
FireDroX <firedrox@gmail.com>
2026-06-06 17:03:51 +0200

    Enhance user management: add user activation toggle, improve user update functionality, and implement user deactivation logic; add styles for user interface

--- api/admin/users.php
+++ api/admin/users.php
@@ -8,10 +8,10 @@ if (
     !isset($_SESSION['user']) || 
     $_SESSION['user']['role'] != 1 ||
     !$_SESSION['user']['is_active']
-  ) {
-  http_response_code(403);
-  echo json_encode(['error' => 'Forbidden']);
-  exit;
+) {
+    http_response_code(403);
+    echo json_encode(['error' => 'Forbidden']);
+    exit;
 }
 
 $id = isset($_POST['id_user']) ? (int) $_POST['id_user'] : 0;
@@ -19,6 +19,7 @@ $pseudo = trim($_POST['pseudo'] ?? '');
 $mail = trim($_POST['mail'] ?? '');
 $mdp = trim($_POST['mdp'] ?? '');
 $role = isset($_POST['id_role']) ? (int) $_POST['id_role'] : 0;
+$active = isset($_POST['active']) ? (int) $_POST['active'] : 1;
 
 function checkInputs($p, $m, $r, $pdo) {
   if ($p === '' || $m === '') {
@@ -28,7 +29,8 @@ function checkInputs($p, $m, $r, $pdo) {
 
   $stmt = $pdo->prepare("SELECT * FROM roles WHERE id_role = :id");
   $stmt->execute([':id' => $r]);
-  if (!$stmt->fetch()) {
+
+  if (!$stmt->fetch(PDO::FETCH_ASSOC)) {
     http_response_code(400);
     echo json_encode(['error' => 'Role non existant !']);
     exit;
@@ -37,18 +39,68 @@ function checkInputs($p, $m, $r, $pdo) {
 
 try {
   if ($id > 0) {
-    if ($pseudo === '' && $mail === '' && $mdp === '') {
-      $stmt = $pdo->prepare("UPDATE users SET deactivated = Now() WHERE id_users = :id");
-      $stmt->execute([':id' => $id]);
-      echo json_encode(['success' => true, 'message' => 'Utilisateur Désactivé !']);
-      exit;
-    }
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE id_users = :id");
+    $stmt->execute([':id' => $id]);
+    $prevUser = $stmt->fetch(PDO::FETCH_ASSOC);
 
     checkInputs($pseudo, $mail, $role, $pdo);
 
+    if (is_null($prevUser['deactivated']) && $active === 0) {
+      $stmt = $pdo->prepare("
+        UPDATE users
+        SET deactivated = NOW(),
+          pseudo = :pseudo,
+          mail = :mail,
+          mdp = :mdp,
+          id_role = :role
+        WHERE id_users = :id
+      ");
+
+      $stmt->execute([
+        ':pseudo' => $pseudo,
+        ':mail' => $mail,
+        ':mdp' => $mdp,
+        ':role' => $role,
+        ':id' => $id
+      ]);
+
+      echo json_encode([
+        'success' => true,
+        'message' => 'Utilisateur désactivé / modifié'
+      ]);
+      exit;
+    } else if (!is_null($prevUser['deactivated']) && $active === 1) {
+      $stmt = $pdo->prepare("
+        UPDATE users
+        SET deactivated = NULL,
+          pseudo = :pseudo,
+          mail = :mail,
+          mdp = :mdp,
+          id_role = :role
+        WHERE id_users = :id
+      ");
+
+      $stmt->execute([
+        ':pseudo' => $pseudo,
+        ':mail' => $mail,
+        ':mdp' => $mdp,
+        ':role' => $role,
+        ':id' => $id
+      ]);
+
+      echo json_encode([
+        'success' => true,
+        'message' => 'Utilisateur réactivé / modifié'
+      ]);
+      exit;
+    }
+
     $stmt = $pdo->prepare("
       UPDATE users
-      SET pseudo = :pseudo, mail = :mail, mdp = :mdp, id_role = :role
+      SET pseudo = :pseudo,
+        mail = :mail,
+        mdp = :mdp,
+        id_role = :role
       WHERE id_users = :id
     ");
 
@@ -60,34 +112,36 @@ try {
       ':id' => $id
     ]);
 
-    echo json_encode(['success' => true, 'message' => 'Utilisateur mis à jour']);
-  } else {
-    checkInputs($pseudo, $mail, $role, $pdo);
-
-    $stmt = $pdo->prepare("SELECT mail FROM users WHERE mail = :mail");
-    $stmt->execute([
-        ':mail' => $mail
+    echo json_encode([
+      'success' => true,
+      'message' => 'Utilisateur mis à jour'
     ]);
-
-    if ($stmt->fetch()) {
-      echo json_encode(['error' => 'Mail existant !']);
-      exit;
-    }
-
-    $stmt = $pdo->prepare("
-      INSERT INTO users (pseudo, mail, mdp, id_role)
-      VALUES (:pseudo, :mail, :mdp, :role)
-    ");
-
-    $stmt->execute([
-      ':pseudo' => $pseudo,
-      ':mail' => $mail,
-      ':mdp' => password_hash($mdp, PASSWORD_DEFAULT),
-      ':role' => $role,
-    ]);
-
-    echo json_encode(['success' => true, 'message' => 'Utilisateur créé']);
+    exit;
   }
+
+  checkInputs($pseudo, $mail, $role, $pdo);
+
+  $stmt = $pdo->prepare("SELECT mail FROM users WHERE mail = :mail");
+  $stmt->execute([':mail' => $mail]);
+
+  if ($stmt->fetch()) {
+    echo json_encode(['error' => 'Mail existant !']);
+    exit;
+  }
+
+  $stmt = $pdo->prepare("
+    INSERT INTO users (pseudo, mail, mdp, id_role)
+    VALUES (:pseudo, :mail, :mdp, :role)
+  ");
+
+  $stmt->execute([
+    ':pseudo' => $pseudo,
+    ':mail' => $mail,
+    ':mdp' => password_hash($mdp, PASSWORD_DEFAULT),
+    ':role' => $role,
+  ]);
+
+  echo json_encode(['success' => true, 'message' => 'Utilisateur créé']);
 } catch (PDOException $e) {
   http_response_code(500);
   echo json_encode([
--- panel/captcha/index.php
+++ panel/captcha/index.php
@@ -35,6 +35,7 @@ try {
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="styles.css">
   </head>
 
   <body>
