f32376c7dee9bc5bee76dd4daa59b7a632ea1e89
Seoo <153709278+XeTr0S@users.noreply.github.com>
2026-06-19 15:10:07 +0200

    Remove remember_token cookie setting from login processing

--- logout/index.php
+++ logout/index.php
@@ -21,6 +21,13 @@ if (isset($_COOKIE['remember_token'])) {
         time() - 3600,
         '/'
     );
+
+    setcookie(
+        'user_email',
+        '',
+        time() - 3600,
+        '/'
+    );
 }
 
 $_SESSION = [];
@@ -29,31 +36,32 @@ session_destroy();
 
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Déconnexion</title>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Déconnexion</title>
 
-    <link rel="shortcut icon" href="/favicon.png" type="image/png">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="/styles/home.css">
-    <link rel="stylesheet" href="/panel/captcha/styles.css">
-</head>
-<body>
-    <div class="container">
-        <div class="row justify-content-center align-items-center vh-100">
-            <div class="col-md-6">
-                <main class="container card shadow p-4 text-center">
-                    <h1 class="mb-3">Déconnexion réussie</h1>
-                    <p class="mb-4 text-light">
-                        Vous avez bien été déconnecté.
-                    </p>
-                    <a href="/" class="btn btn-primary">
-                        Retour à l'accueil
-                    </a>
-                </main>
+        <link rel="shortcut icon" href="/favicon.png" type="image/png">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="/styles/home.css">
+        <link rel="stylesheet" href="/panel/captcha/styles.css">
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="row justify-content-center align-items-center vh-100">
+                <div class="col-md-6">
+                    <main class="container card shadow p-4 text-center">
+                        <h1 class="mb-3">Déconnexion réussie</h1>
+                        <p class="mb-4 text-light">
+                            Vous avez bien été déconnecté.
+                        </p>
+                        <a href="/" class="btn btn-primary">
+                            Retour à l'accueil
+                        </a>
+                    </main>
+                </div>
             </div>
         </div>
-    </div>
-</body>
+    </body>
 </html>
