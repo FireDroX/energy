fca9fa87043af3d8b17858c44e7041280202cf13
FireDroX <FireDroX@gmail.com>
2026-07-07 11:32:48 +0200

    refactor(git-stats): streamline data fetching and remove caching logic

--- utils/git-stats.php
+++ utils/git-stats.php
@@ -1,61 +1,37 @@
 <?php
 
-$cacheFile = __DIR__ . "/../uploads/github_stats.json";
+$url = "https://api.github.com/repos/FireDroX/energy/stats/contributors";
 
-if (!file_exists($cacheFile) || (time() - $stats["timestamp"]) > 3600) {
+$options = [
+    "http" => [
+        "header" => "User-Agent: Monster-Website\r\n"
+    ]
+];
 
-    $url = "https://api.github.com/repos/FireDroX/energy/stats/contributors";
+$context = stream_context_create($options);
 
-    $options = [
-        "http" => [
-            "header" => "User-Agent: Monster-Website\r\n"
-        ]
-    ];
+$json = file_get_contents($url, false, $context);
 
-    $context = stream_context_create($options);
-    $json = @file_get_contents($url, false, $context);
+$data = json_decode($json, true);
 
-    if ($json !== false) {
+$totalCommits = 0;
+$totalAdded = 0;
+$totalDeleted = 0;
 
-        $data = json_decode($json, true);
+foreach ($data as $contributor) {
 
-        $totalCommits = 0;
-        $totalAdded = 0;
-        $totalDeleted = 0;
+    $totalCommits += $contributor["total"];
 
-        foreach ($data as $contributor) {
-
-            $totalCommits += $contributor["total"];
-
-            foreach ($contributor["weeks"] as $week) {
-                $totalAdded += $week["a"];
-                $totalDeleted += $week["d"];
-            }
-        }
-
-        $stats = [
-            "commits"  => $totalCommits,
-            "added"    => $totalAdded,
-            "deleted"  => $totalDeleted,
-            "timestamp"=> time()
-        ];
-
-        file_put_contents(
-            $cacheFile,
-            json_encode($stats, JSON_PRETTY_PRINT)
-        );
+    foreach ($contributor["weeks"] as $week) {
+        $totalAdded += $week["a"];
+        $totalDeleted += $week["d"];
     }
-}
 
-if (file_exists($cacheFile)) {
-    return json_decode(file_get_contents($cacheFile), true);
 }
 
 return [
-    "commits" => 0,
-    "added" => 0,
-    "deleted" => 0,
-    "timestamp" => 0
+    "commits" => $totalCommits,
+    "added" => $totalAdded,
+    "deleted" => $totalDeleted
 ];
-
 ?>
